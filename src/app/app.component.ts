704867e0c1c113fb80626b9a2e3af21f764e9d8b
Kanrya <nyataraxis@gmail.com>
2019-01-26 22:53:41 +0300

    added store to app module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,11 +16,11 @@ export class AppComponent {
   videos$: Observable<Video[]>;
   selected$: Observable<any>;
   title = 'ngrx-video-list';
-  constructor(private store: Store<fromRoot.State>){
+  constructor(private store: Store<fromRoot.State>) {
     this.videos$ = store.select(fromRoot.getAllVideos);
     this.selected$ = store.select(fromRoot.getSelectedVideo);
   }
-  onSelect(id: number){
+  onSelect(id: number) {
     this.store.dispatch(new videoAction.Select(id));
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { StoreModule } from '@ngrx/store';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { VideoListComponent } from './components/video-list/video-list.component';
 import { VideoItemComponent } from './components/video-item/video-item.component';
 import { VideoSelectedComponent } from './components/video-selected/video-selected.component';
+import { reducers, metaReducers } from './store/reducers/index';
 
 @NgModule({
   declarations: [
     AppComponent,
     VideoListComponent,
     VideoItemComponent,
-    VideoSelectedComponent
+    VideoSelectedComponent,
+    StoreModule.forRoot(reducers, {metaReducers})
   ],
   imports: [
     BrowserModule,
