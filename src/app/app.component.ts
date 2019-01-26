bf893e1bfadbceebbf471baa3df252aaaa266f34
Kanrya <nyataraxis@gmail.com>
2019-01-26 22:15:22 +0300

    added main app component description

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { Video } from './models/videos';
+import { Store } from '@ngrx/store';
+
+import * as fromRoot from './store/reducers';
+import * as videoAction from './store/actions/videos';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +13,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  videos$: Observable<Video[]>;
+  selected$: Observable<any>;
   title = 'ngrx-video-list';
+  constructor(private store: Store<fromRoot.State>){
+    this.videos$ = store.select(fromRoot.getAllVideos);
+    this.selected$ = store.select(fromRoot.getSelectedVideo);
+  }
+  onSelect(id: number){
+    this.store.dispatch(new videoAction.Select(id));
+  }
 }
